48c0ce3da439ac3050149facca9b6ae5b08f4db0
Yassine-Zaazaa <72793135+Yassine-Zaazaa@users.noreply.github.com>
2021-03-29 09:16:28 +0100

    Update iterative_fibo.c

--- TP1/EX1/iterative_fibo.c
+++ TP1/EX1/iterative_fibo.c
@@ -2,5 +2,12 @@
 
 int iterative_fibo(int n)
 {
-    // write your iterative_fibo code here
+  int TAB[n+2];
+  int i;
+  f[0] = 0;
+  f[1] = 1;
+ 
+  for (i = 2; i <= n; i++)
+      f[i] = f[i-1] + f[i-2];
+  return f[n];
 }
