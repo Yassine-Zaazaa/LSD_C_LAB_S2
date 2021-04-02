207ce2d6a7beb7c3a6ae3a1b6635dd4b7ed2f7e6
Yassine-Zaazaa <yassine.zaazaa@um6p.ma>
2021-04-02 16:01:05 +0100

    iterative_fibo

--- TP1/EX1/iterative_fibo.c
+++ TP1/EX1/iterative_fibo.c
@@ -2,6 +2,7 @@
 
 int iterative_fibo(int n)
 {
+<<<<<<< HEAD
       int TAB[n+2];
   	int i;
  	TAB[0] = 0;
@@ -10,4 +11,14 @@ int iterative_fibo(int n)
 	for (i = 2; i <= n; i++)
       	TAB[i] = TAB[i-1] + TAB[i-2];
   	return TAB[n];
+=======
+  int TAB[n+2];
+  int i;
+  TAB[0] = 0;
+  TAB[1] = 1;
+ 
+  for (i = 2; i <= n; i++)
+      TAB[i] = TAB[i-1] + TAB[i-2];
+  return TAB[n];
+>>>>>>> 580cac0762b7aa9b1d1c30545e3416d186090eae
 }
