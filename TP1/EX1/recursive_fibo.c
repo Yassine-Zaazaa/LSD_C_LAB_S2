8c347c21d90036e1a9c317372219caa045158b15
Yassine-Zaazaa <yassine.zaazaa@um6p.ma>
2021-04-02 16:00:45 +0100

    recursive_fibo

--- TP1/EX1/recursive_fibo.c
+++ TP1/EX1/recursive_fibo.c
@@ -2,9 +2,17 @@
 
 int recursive_fibo(int n)
 {
+<<<<<<< HEAD
 	if(n == 0)
       	return 0;
 	if(n == 1)
       	return 1;
       return (recursive_fibo(n-1) + recursive_fibo(n-2));
+=======
+    if(n == 0)
+        return 0;
+    if(n == 1)
+        return 1;
+    return (recursive_fibo(n-1) + recursive_fibo(n-2));
+>>>>>>> 580cac0762b7aa9b1d1c30545e3416d186090eae
 }
